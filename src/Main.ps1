4c6b5cae68a8a74fdc9415d9848198e2946c5b13
tom <Tomvla49@gmail.com>
2026-03-28 14:01:03 +0100

    orchestration started empty files and structure added

--- src/Main.ps1
+++ src/Main.ps1
@@ -7,3 +7,40 @@
 . "$PSScriptRoot\processing\RiskEngine.ps1"
 . "$PSScriptRoot\output\Reports.ps1"
 . "$PSScriptRoot\output\Mail.ps1"
+
+try {
+    Write-Host "[STEP] Starting Devices Monitor" -ForegroundColor Cyan
+
+    #========================================
+    #                AUTH
+    #========================================
+
+    $accessToken = Get-Graphtoken -TenantId $TenantId -ClientId -ClientSecret $ClientSecret
+
+    $headers = @{
+        Authorization = "Bearer $accessToken"
+    }
+
+    #========================================
+    #               ENTRA
+    #========================================
+    
+    $entraData = Get-EntraDevices `
+            -Headers $headers `
+            -RawDataPath $RawDataPath `
+            -ProcessedDataPath $ProcessedDataPath `
+
+    $entraByDeviceId = $entraData.BydeviceId
+    $entraByDisplayName = $entraData.BydisplayName
+
+    #Intune
+
+    $intuneData = Get-IntuneDevices `
+            -Headers $headers `
+            -EntraByDeviceId $entraByDeviceId
+            
+
+}
+catch {
+    
+}
